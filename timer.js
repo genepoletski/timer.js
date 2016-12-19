c3cccdd7eac01f95458cc68a4a274a8c1b981054
eugene poletski <web.developer.poletski@gmail.com>
2016-12-20 00:27:54 +0300

    Add chaining

--- timer.js
+++ timer.js
@@ -1,32 +1,48 @@
 var timer = function() {
   var
     timers = {},
-    set, del, isSet;
+    timerPublic = {},
+    set, then, del, isSet;
 
-  set = function( timerID, callback, timeout) {
+  function Timer( timerID, callback, timeout ) {
+    var self = this;
+    this.timerID = timerID;
+    this._resolves = [];
+    this.timer =  setTimeout( function() {
+      callback();
+      self._resolves.forEach( function( fn ) {
+        fn();
+      } );
+    }, timeout );
+  }
+
+  Timer.prototype = timerPublic;
+
+  timerPublic.set = set = function( timerID, callback, timeout) {
     if ( isSet( timerID ) ) {
       del( timerID );
     }
-    timers[ timerID ] = setTimeout( function() {
-      callback();
-    }, timeout );
+    timers[ timerID ] = new Timer( timerID, callback, timeout );
+    return timers[ timerID ];
   };
 
-  del = function( timerID ) {
+  timerPublic.del = del = function( timerID ) {
     if ( isSet( timerID ) ) {
-      clearTimeout( timers[ timerID ] );
+      clearTimeout( timers[ timerID ].timer );
+      delete timers[ timerID ];
       return true;
     }
     return false;
   };
 
-  isSet = function( timerID ) {
+  timerPublic.then = then = function( callback, timeout ) {
+    this._resolves.push( callback );
+    return timers[ this.timerID ];
+  };
+
+  timerPublic.isSet = isSet = function( timerID ) {
     return timers.hasOwnProperty( timerID );
   };
 
-  return {
-    set: set,
-    del: del,
-    isSet: isSet
-  };
+  return timerPublic;
 };
