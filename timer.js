67cd48a90442b5b820a109a70bf31c3e2fa33007
eugene poletski <web.developer.poletski@gmail.com>
2016-12-20 12:34:08 +0300

    Add queue

--- timer.js
+++ timer.js
@@ -1,48 +1,72 @@
-var timer = function() {
-  var
-    timers = {},
-    timerPublic = {},
-    set, then, del, isSet;
+  function timer() {
+    var
+        timers = {},
+        timerPublic = {},
+        set, then, del, isSet;
 
-  function Timer( timerID, callback, timeout ) {
-    var self = this;
-    this.timerID = timerID;
-    this._resolves = [];
-    this.timer =  setTimeout( function() {
-      callback();
-      self._resolves.forEach( function( fn ) {
-        fn();
-      } );
-    }, timeout );
-  }
+      function Timer( timerID, callback, timeout ) {
+        var self = this;
 
-  Timer.prototype = timerPublic;
+        this.timerID = timerID;
+        this.__queue = [];
+        this.__queueTimer = null;
+        this.__queueFn = null;
 
-  timerPublic.set = set = function( timerID, callback, timeout) {
-    if ( isSet( timerID ) ) {
-      del( timerID );
+        this.timer =  setTimeout( function() {
+          callback();
+          if ( self.__queue.length > 0 ) {
+            self.__resolveQueue( self.__queue );
+          }
+        }, timeout );
+
+        this.__resolveQueue = function( queue ) {
+          var timeout;
+          if ( queue.length === 0 ) return;
+          self.__queueFn = queue[0];
+          timeout = queue[1] || 0;
+
+          self.__queueTimer = setTimeout(
+            function() {
+              self.__queueFn();
+              self.__queue.shift();
+              if (timeout > 0) self.__queue.shift();
+              self.__resolveQueue( self.__queue );
+            }, timeout );
+        };
+
+        this.__clearQueue = function() {
+          
+        };
+      }
+
+      Timer.prototype = timerPublic;
+
+      timerPublic.set = set = function( timerID, callback, timeout) {
+        if ( isSet( timerID ) ) {
+          del( timerID );
+        }
+        timers[ timerID ] = new Timer( timerID, callback, timeout );
+        return timers[ timerID ];
+      };
+
+      timerPublic.del = del = function( timerID ) {
+        if ( isSet( timerID ) ) {
+          clearTimeout( timers[ timerID ].timer );
+          delete timers[ timerID ];
+          return true;
+        }
+        return false;
+      };
+
+      timerPublic.then = then = function( callback, timeout ) {
+        this.__queue.push( callback );
+        this.__queue.push( timeout );
+        return timers[ this.timerID ];
+      };
+
+      timerPublic.isSet = isSet = function( timerID ) {
+        return timers.hasOwnProperty( timerID );
+      };
+
+      return timerPublic;
     }
-    timers[ timerID ] = new Timer( timerID, callback, timeout );
-    return timers[ timerID ];
-  };
-
-  timerPublic.del = del = function( timerID ) {
-    if ( isSet( timerID ) ) {
-      clearTimeout( timers[ timerID ].timer );
-      delete timers[ timerID ];
-      return true;
-    }
-    return false;
-  };
-
-  timerPublic.then = then = function( callback, timeout ) {
-    this._resolves.push( callback );
-    return timers[ this.timerID ];
-  };
-
-  timerPublic.isSet = isSet = function( timerID ) {
-    return timers.hasOwnProperty( timerID );
-  };
-
-  return timerPublic;
-};
